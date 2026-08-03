d77ea9486beb748c9377586a7499157b1c477b1d
Saravana20-S <iamsaravanan1347@gmail.com>
2026-08-03 19:49:44 +0530

    JDBC-UC10: Use Cases Verified

--- test/java/com/bridgelabz/advemployeepayroll/service/EmployeePayrollServiceTest.java
+++ test/java/com/bridgelabz/advemployeepayroll/service/EmployeePayrollServiceTest.java
@@ -53,21 +53,87 @@ class EmployeePayrollServiceTest {
     }
     
 
+
+
+
+    //UC10
+    @Test
+    void givenPayrollDatabase_WhenRetrieved_ShouldReturnEmployeeList() {
+
+        List<EmployeePayroll> list =
+                payrollService.getEmployeePayrollList();
+
+        Assertions.assertFalse(list.isEmpty());
+    }
+
+    @Test
+    void givenNewSalary_WhenUpdated_ShouldSyncWithDatabase() {
+
+        EmployeePayroll employee =
+                payrollService.updateEmployeeSalary(
+                        "Terisa",
+                        3000000);
+
+        Assertions.assertTrue(
+                payrollService
+                        .checkEmployeePayrollInSync(employee));
+    }
+
+    @Test
+    void givenDateRange_WhenRetrieved_ShouldReturnEmployees() {
+
+        List<EmployeePayroll> employees =
+                payrollService.getEmployeesByDateRange(
+
+                        LocalDate.of(2018,1,1),
+
+                        LocalDate.now()
+
+                );
+
+        Assertions.assertFalse(
+                employees.isEmpty());
+    }
+
+
+    @Test
+    void givenEmployees_WhenStatisticsComputed_ShouldReturnGenderStatistics() {
+
+        List<PayrollStatistics> statistics =
+                payrollService
+                        .getPayrollStatisticsByGender();
+
+        Assertions.assertFalse(
+                statistics.isEmpty());
+    }
+
+
     @Test
     void givenNewEmployee_WhenAdded_ShouldSyncWithDatabase() {
 
         EmployeePayroll employee =
                 payrollService.addEmployee(
+
                         "Kumar",
+
                         'M',
-                        5000000,
+
+                        6000000,
+
                         LocalDate.now(),
-                        1);
+
+                        List.of(1,2)
+
+                );
 
         Assertions.assertNotNull(employee);
 
+        Assertions.assertEquals(
+                2,
+                employee.getDepartments().size());
+
         Assertions.assertTrue(
-                payrollService.checkEmployeePayrollInSync(employee)
-        );
+                payrollService
+                        .checkEmployeePayrollInSync(employee));
     }
 }
