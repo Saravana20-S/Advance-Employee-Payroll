403f1841337237c23f59ee5b91e5f85574ca7810
Saravana20-S <iamsaravanan1347@gmail.com>
2026-08-03 11:51:18 +0530

    UC1: Created Database

---
+++ main/java/com/bridgelabz/advemployeepayroll/Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.advemployeepayroll;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //UC1
+        System.out.println("DataBase Created");
+    }
+}
